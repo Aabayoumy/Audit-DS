21c2d66ac5ee509e046b71e50e929aa7878016f6
Ahmed Bayoumy <abayoumy@outlook.com>
2025-07-09 14:25:23 +0300

    export-adinfo change txt files to export csv

--- AuditModule/public/Export-ADInfo.ps1
+++ AuditModule/public/Export-ADInfo.ps1
@@ -20,14 +20,12 @@ function Export-ADInfo {
     $null = Start-Transcript -Path "$OutputPath\transcript.txt"
     $ForestData = ($Forest | Select-Object Name, RootDomain,Domains, ForestMode, SchemaMaster, DomainNamingMaster, Sites )
     ($ForestData | Out-String).Trim() > "$OutputPath\_ADInfo_$($ADDomain.DNSRoot).txt"   
-    $ForestData | Export-Report -OutputPath $OutputPath -Title "Forest " `
-        -DomainInfo $Forest.Name
 
     $Subnets = (Get-ADReplicationSubnet -Filter * | Select-Object Name, Site | Sort-Object Site )
-    ($Subnets  | Format-Table | Out-String).Trim() >> "$OutputPath\Subnets.txt"
+    $Subnets | Export-Csv -Path "$OutputPath\Subnets.csv" -NoTypeInformation
 
     # Pass-NotReq Users
-    Get-ADUser -LDAPFilter "(userAccountControl:1.2.840.113556.1.4.803:=32)" -Properties userAccountControl | Select-Object samaccountname,useraccountcontrol | Export-Csv -Path "$OutputPath\users-pass-notreq.csv"
+    Get-ADUser -LDAPFilter "(userAccountControl:1.2.840.113556.1.4.803:=32)" -Properties userAccountControl | Select-Object samaccountname,useraccountcontrol | Export-Csv -Path "$OutputPath\users-pass-notreq.csv" -NoTypeInformation
 
     Add-Content -Path "$OutputPath\_ADInfo_$($ADDomain.DNSRoot).txt" -Value "*`r`n#Domain $($ADDomain.DNSRoot)"
     ($ADDomain | Select-Object NetBIOSName, DNSRoot, DomainMode, PDCEmulator, InfrastructureMaster, RIDMaster | Out-String).Trim() >> "$OutputPath\_ADInfo_$($ADDomain.DNSRoot).txt"
@@ -38,23 +36,23 @@ function Export-ADInfo {
     repadmin /showrepl *   /csv > "$OutputPath\showrepl.csv"
     Gpresult /h "$($OutputPath)\$($env:computername)_GPResult.html"
     Auditpol /get /category:* > "$($OutputPath)\$($env:computername)_Audit.txt"
-    (Get-ADForest -Current LoggedOnUser).Domains | %{ Get-ADDefaultDomainPasswordPolicy -Identity $_ } > "$OutputPath\DomainPasswordPolicy.txt"
+    (Get-ADForest -Current LoggedOnUser).Domains | %{ Get-ADDefaultDomainPasswordPolicy -Identity $_ } | Export-Csv -Path "$OutputPath\DomainPasswordPolicy.csv" -NoTypeInformation
     nltest /DOMAIN_TRUSTS  > "$OutputPath\Trust.txt"
-    Get-ADUser -Filter {PasswordNeverExpires -eq $true} | Select-Object samaccountname,enabled,DistinguishedName > "$OutputPath\users-password-never-expires.txt"
-    Get-ADUser -Filter {(adminCount -ne 0 ) -and (serviceprincipalname -like "*") }  -Property samaccountname, DistinguishedName, serviceprincipalname, enabled | Select-Object samaccountname, DistinguishedName, serviceprincipalname, enabled > "$OutputPath\users-with-spn.txt" # users with spn
-    Get-ADUser -Filter { TrustedForDelegation -eq $True } -Property DistinguishedName, ServicePrincipalName, TrustedForDelegation | Select-Object DistinguishedName, ServicePrincipalName, TrustedForDelegation , enabled > "$OutputPath\users-with-unconstrained-delegation.txt" # users with  unconstrained delegation
-    Get-ADComputer -Filter { (TrustedForDelegation -eq $True) -and (PrimaryGroupID -ne 516) } -Property DistinguishedName, ServicePrincipalName, TrustedForDelegation | Select-Object DistinguishedName, ServicePrincipalName, TrustedForDelegation , enabled > "$OutputPath\servers-with-unconstrained-delegation.txt" # servers with  unconstrained delegation
+    Get-ADUser -Filter {PasswordNeverExpires -eq $true} | Select-Object samaccountname,enabled,DistinguishedName | Export-Csv -Path "$OutputPath\users-password-never-expires.csv" -NoTypeInformation
+    Get-ADUser -Filter {(adminCount -ne 0 ) -and (serviceprincipalname -like "*") }  -Property samaccountname, DistinguishedName, serviceprincipalname, enabled | Select-Object samaccountname, DistinguishedName, serviceprincipalname, enabled | Export-Csv -Path "$OutputPath\users-with-spn.csv" -NoTypeInformation # users with spn
+    Get-ADUser -Filter { TrustedForDelegation -eq $True } -Property DistinguishedName, ServicePrincipalName, TrustedForDelegation | Select-Object DistinguishedName, ServicePrincipalName, TrustedForDelegation , enabled | Export-Csv -Path "$OutputPath\users-with-unconstrained-delegation.csv" -NoTypeInformation # users with  unconstrained delegation
+    Get-ADComputer -Filter { (TrustedForDelegation -eq $True) -and (PrimaryGroupID -ne 516) } -Property DistinguishedName, ServicePrincipalName, TrustedForDelegation | Select-Object DistinguishedName, ServicePrincipalName, TrustedForDelegation , enabled | Export-Csv -Path "$OutputPath\servers-with-unconstrained-delegation.csv" -NoTypeInformation # servers with  unconstrained delegation
     Get-ACL "AD:\$DN" | Select-Object -ExpandProperty Access | Where-Object {($_.ObjectType -eq '89e95b76-444d-4c62-991a-0facbeda640c' -or $_.ObjectType -eq '1131f6aa-9c07-11d1-f79f-00c04fc2dcd2')} | Select-Object IdentityReference > "$OutputPath\users-with-dcsync-privilege.txt" # users withdcsync rights
     $DaysInactive = 180
     $time = (Get-Date).Adddays(-($DaysInactive))
-    Get-ADUser -Filter '(LastLogonTimestamp -lt $time)' -Properties enabled,PasswordLastSet,LastLogonTimestamp | Format-Table Name,enabled,PasswordLastSet,@{N="LastLogonTimestamp";E={[datetime]::FromFileTime($_.LastLogonTimestamp)}}  >  "$OutputPath\Inactive_Users.txt"
+    Get-ADUser -Filter '(LastLogonTimestamp -lt $time)' -Properties enabled,PasswordLastSet,LastLogonTimestamp | Select-Object Name,enabled,PasswordLastSet,@{N="LastLogonTimestamp";E={[datetime]::FromFileTime($_.LastLogonTimestamp)}} | Export-Csv -Path "$OutputPath\Inactive_Users.csv" -NoTypeInformation
 
-    Get-ADComputer -Filter { (LastLogonTimestamp -lt $time -or LastLogonTimestamp -notlike "*") } -Properties LastLogonTimestamp, WhenCreated, PasswordLastSet | Select-Object Name, WhenCreated, PasswordLastSet, @{N="LastLogonTimestamp";E={if ($_.LastLogonTimestamp) {[datetime]::FromFileTime($_.LastLogonTimestamp)} else {"Never"}}} > "$OutputPath\Inactive_Computers.txt"
+    Get-ADComputer -Filter { (LastLogonTimestamp -lt $time -or LastLogonTimestamp -notlike "*") } -Properties LastLogonTimestamp, WhenCreated, PasswordLastSet | Select-Object Name, WhenCreated, PasswordLastSet, @{N="LastLogonTimestamp";E={if ($_.LastLogonTimestamp) {[datetime]::FromFileTime($_.LastLogonTimestamp)} else {"Never"}}} | Export-Csv -Path "$OutputPath\Inactive_Computers.csv" -NoTypeInformation
     
     # Define the list of special user account patterns to check
     $SpecialUserPatterns = @(
         'krb*',
-        'Azure*',
+        'AZURE*',
         'MSOL*'
         # Add more patterns here if needed in the future
     )
